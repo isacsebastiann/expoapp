e679691770035b05ca3c603232da3258799378f9
Isaac Sebastián Guerra <isguerra@sudamericano.edu.ec>
2024-01-20 20:24:37 -0500

    Update Expo App
    Change Image From Gallery Function

--- components/Button.js
+++ components/Button.js
@@ -1,9 +1,29 @@
 import { StyleSheet, View, Pressable, Text } from 'react-native';
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
-export default function Button({ label }) {
+export default function Button({ label, theme, onPress }) {
+  if (theme === "primary") {
+    return (
+      <View style={[styles.buttonContainer, { borderWidth: 4, borderColor: "#ffd33d", borderRadius: 18 }]}>
+        <Pressable
+          style={[styles.button, { backgroundColor: '#fff' }]}
+          onPress={onPress}
+        >
+          <FontAwesome
+            name="picture-o"
+            size={18}
+            color="#25292e"
+            style={styles.buttonIcon}
+          />
+          <Text style={[styles.buttonLabel, { color: "#25292e" }]}>{label}</Text>
+        </Pressable>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.buttonContainer}>
-      <Pressable style={styles.button} onPress={() => alert('You pressed a button.')}>
+      <Pressable style={styles.button} onPress={onPress}>
         <Text style={styles.buttonLabel}>{label}</Text>
       </Pressable>
     </View>
@@ -12,26 +32,35 @@ export default function Button({ label }) {
 
 const styles = StyleSheet.create({
   buttonContainer: {
-    width: 320,
-    height: 68,
     marginHorizontal: 20,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 3,
+    marginBottom: 12,
+    elevation: 3,
+    borderRadius: 20,
+    overflow: 'hidden',
   },
   button: {
-    borderRadius: 10,
+    backgroundColor: '#007bff', // A pleasant blue shade
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    borderRadius: 20,
     width: '100%',
-    height: '100%',
+    flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
-    flexDirection: 'row',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
   },
   buttonIcon: {
-    paddingRight: 8,
+    paddingRight: 10,
   },
   buttonLabel: {
     color: '#fff',
-    fontSize: 16,
+    fontSize: 18,
+    fontWeight: 'bold',
   },
 });
